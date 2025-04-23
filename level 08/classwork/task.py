7a2606f0d92a331a0adbc6d846f96deae62c6095
luka170321 <lebanidzesalome@gmail.com>
2025-04-23 20:06:17 +0400

    commit

---
+++ level 08/classwork/task.py
@@ -0,0 +1,13 @@
+print( 10 + 10 ) # integer (int) 1)
+
+
+print( 1.2 + 1.5 ) # float 2)
+
+
+print("lu + ka") # string (str) 3)
+
+
+print( 1000 - 500 - 250 - 50) # integer (int) 4)
+
+
+print( 1.7 + 1.7 ) # float 5)
---
+++ level 10/classwork/task.py
@@ -0,0 +1,25 @@
+# გამოიყენე ყველა შედარებითი ოპერატორი და გამოიტანე  True ან False
+
+print(10 < 20) # True
+print(20 < 10) # False
+print(30 < 40) # True
+
+print(17 > 2) # True
+print(21 > 25) # False
+print(14 > 4) # True
+
+print(17 == 17) # True
+print(1000 == 1) # False
+print(7643786598347 == 7643786598347) # True
+
+print(112 <= 113) # True
+print(1000 <= 999) # False
+print(999 <= 1000000000000000000000000000000000000) # True
+
+print(100000000000 >= 7) # True
+print(10 >= 456) # False
+print(300 >= 300) # True
+
+print(10 != 999) # True
+print(1 != 1) # False
+print(123 != 789) # True
